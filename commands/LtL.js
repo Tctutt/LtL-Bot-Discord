00895191a2687330af44005ac7bb332e83930681
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-03 20:07:15 -0800

    testing slash command stuff

---
+++ commands/LtL.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('LtL')
+        .setDescription('The main command for the Bot.')
+        .addStringOption(option =>
+            option.setName('SubCommand')
+                .setDescription('Subcommand for the command')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Setup', value: 'something here' },
+                    { name: 'Default', value: 'something here' },
+                    { name: 'Default', value: 'something here' },
+                )),
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
+};
