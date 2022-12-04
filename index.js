1c216f67cf542c3ad80860cc5054d32e579ba4a3
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-03 18:18:45 -0800

    add slash command functionality

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 // Main discord bot file.
 // Requirements and Variables
 require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
 const keepAlive = require(`./server`);
 const { Client } = require('discord.js');
 const client = new Client({ intents: 32767 });
 
-// Array of Command objects
-const cmds = [{
+//New Commands Collection
+client.commands = new Collection();
+
+// Defined Commands Location
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Registering the Commands with Discord
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+
+/*const cmds = [{
     name: `ping`, // Command name
     description: `Ping!`, // Command description
     async execute(interaction) { // Execute function
@@ -15,9 +36,10 @@ const cmds = [{
         });
     }
 }]
+*/
 
 // Interaction Create Event
-client.on('interactionCreate', async interaction => {
+/*client.on('interactionCreate', async interaction => {
     if (interaction.isCommand()) {
         await cmds.forEach(async command => {
             if (interaction.commandName == command.name) {
@@ -30,13 +52,31 @@ client.on('interactionCreate', async interaction => {
         });
     }
 });
+*/
 
 // Ready Event
 client.on('ready', async () => {
-    console.log(`Testing bot is now online successfully!`);
+    console.log(`The Bot is now online successfully!`);
     await client.guilds.cache
         .get(process.env['serverId'])
-        .commands.set(cmds);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
 // Bot Login
