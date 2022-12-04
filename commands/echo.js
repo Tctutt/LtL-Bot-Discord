bc3da832665157b8588c88ef2c7d22a57b803b01
Tctutt <tylertuttle@FixitFeixjrsMBP.hsd1.ca.comcast.net>
2022-12-04 09:17:32 -0800

    added test cmd

--- commands/echo.js
+++ commands/echo.js
@@ -1,18 +1,15 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, SlashCommandStringOption } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-    .setName('echo')
-    .setDescription('Replies with your input!')
-    .addStringOption(option =>
-        option.setName('input')
-            .setDescription('The input to echo back')
-            // Ensure the text will fit in an embed description, if the user chooses that option
-            .setMaxLength(2000))
-    .addChannelOption(option =>
-        option.setName('channel')
-            .setDescription('The channel to echo into')
-            // Ensure the user can only select a TextChannel for output
-            .addChannelTypes(ChannelType.GuildText))
-    .addBooleanOption(option =>
-        option.setName('embed')
-            .setDescription('Whether or not the echo should be embedded'));
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true)),
+    async execute(interaction) {
+        const input = interaction.options.getString("input")
+        await interaction.reply(input)
+    },
+};
