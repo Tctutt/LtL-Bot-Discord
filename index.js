e95c84350dd61742fc405b61b20b415371edafc4
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-03 19:08:21 -0800

    stillllllll trying to fix error

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const mapp = new Map();
 const keepAlive = require(`./server`);
 const { Client } = require('discord.js');
 const client = new Client({ intents: 32767 });
 
 //New Commands Collection
-client.commands = new mapp.Collection();
+client.commands = new Collection();
 
 // Defined Commands Location
 const commandsPath = path.join(__dirname, 'commands');
