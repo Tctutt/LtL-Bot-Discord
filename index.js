d16254bf926c07f6cf3cd58f8fa02630b8244a98
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-04 20:31:28 -0800

    trying to fix slash cmd registering/cleaned up index.js

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
+    client.commands.set(command.data.name, command);
 }
 
 
@@ -64,12 +59,9 @@ client.on('ready', async () => {
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
+    if (!command) return;
 
     try {
         await command.execute(interaction);
