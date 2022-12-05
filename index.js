8d5f2dfa28a7cd8a6b8629285ea34560e14c77af
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-04 20:35:37 -0800

    cleaned up index.js

--- index.js
+++ index.js
@@ -18,36 +18,13 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    client.commands.set(command.data.name, command);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
 }
-
-
-/*const cmds = [{
-    name: `ping`, // Command name
-    description: `Ping!`, // Command description
-    async execute(interaction) { // Execute function
-        await interaction.reply({
-            content: `Pong.`
-        });
-    }
-}]
-*/
-
-// Interaction Create Event
-/*client.on('interactionCreate', async interaction => {
-    if (interaction.isCommand()) {
-        await cmds.forEach(async command => {
-            if (interaction.commandName == command.name) {
-                try {
-                    await command.execute(interaction);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        });
-    }
-});
-*/
 
 // Ready Event
 client.on('ready', async () => {
@@ -59,9 +36,12 @@ client.on('ready', async () => {
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) return;
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
 
     try {
         await command.execute(interaction);
