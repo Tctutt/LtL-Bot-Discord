c26fb2450df8cf2702eae757226d857987600734
Tyler Tuttle <tc.tutt@icloud.com>
2022-12-04 20:38:04 -0800

    added main/test command /ltl

--- commands/ltl.js
+++ commands/ltl.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('test')
+        .setName('ltl')
         .setDescription('Test'),
     async execute(interaction) {
         await interaction.reply('This worked!');
