0a929fb087bba6d701b5da07a2c9cb7b23348bbe
Tctutt <you@example.com>
2022-12-06 14:59:31 -0800

    stuff

--- commands/ltl.js
+++ commands/ltl.js
@@ -3,17 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ltl')
-        .setDescription('The main command for the Bot.')
-        .addStringOption(option =>
-            option.setName('SubCommand')
-                .setDescription('Subcommand for the command')
-                .setRequired(true)
-                .addChoices(
-                    { name: 'setup', value: 'something here' },
-                    { name: 'default', value: 'something here' },
-                    { name: 'default', value: 'something here' },
-                )),
+        .setDescription('Test'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        await interaction.reply('This Worked!');
     },
 };
